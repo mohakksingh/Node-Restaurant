1803875a6b95b63566fc8576c485c32a6d3016a8
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-04-15 00:29:25 +0530

    Authentication and authorisation done

--- models/person.js
+++ models/person.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-
+const bcrypt =require('bcrypt')
 const personSchema=new mongoose.Schema({
     name:{
         type:String,
@@ -28,9 +28,44 @@ const personSchema=new mongoose.Schema({
     salary:{
         type:Number,
         required:true
+    },
+    username:{
+        required:true,
+        type:String
+    },
+    password:{
+        required:true,
+        type:String
     }
 })
 
+personSchema.pre('save',async function(next){
+    const person=this
+
+    //hash the password only if it has been modified (or is new)
+    if(!person.isModified('password'))return next()
+    try{
+        //hash password generate
+        const salt=await bcrypt.genSalt(10)
+
+        //hash password
+        const hashedPassword=await bcrypt.hash(person.password,salt)
+
+        person.password=hashedPassword
+        next()
+    }catch(e){
+        return next(e)
+    }
+})
+
+personSchema.methods.comparePassword=async function(candidatePassword){
+    try{
+        const isMatch=await bcrypt.compare(candidatePassword,this.password)
+        return isMatch
+    }catch(e){
+        throw e
+    }
+}
 //Create Person Model
 const Person=mongoose.model('Person',personSchema)
 
--- server.js
+++ server.js
@@ -3,24 +3,38 @@ const app=express()
 const db=require('./db')
 const bodyParser=require('body-parser')
 require('dotenv').config()
+const passport=require('./auth')
+
 app.use(bodyParser.json())
+const PORT=process.env.PORT||3000
+
+//middleware
+const logRequest=(req,res,next)=>{
+    console.log(`${new Date().toLocaleString()} Request Made to : ${req.originalUrl}`);
+    next()
+}
+
+
+app.use(logRequest)
 
 
 
+app.use(passport.initialize())
 
-app.get('/',(req,res)=>{
+const localAuthMiddleware=passport.authenticate('local',{session:false})
+app.get('/',function(req,res){
     res.send('Welcome to my hotel... How can I help you?,we have a list of menus')
 })
 
 
 
 const personRoutes=require('./routes/personRoutes')
-app.use('/person',personRoutes)
+app.use('/person',localAuthMiddleware,personRoutes)
 
 const menuItemRoutes=require('./routes/menuItemRoutes')
 app.use('/menu',menuItemRoutes)
 
-const PORT=process.env.PORT||3000
+
 
 app.listen(PORT,()=>{
     console.log('listening on port 3000');
